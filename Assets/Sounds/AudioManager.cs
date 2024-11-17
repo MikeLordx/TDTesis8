bbea5fe561d139755ec3cd4eec1df0126293c2a3
Miguel Alejandro Santiago Perez <miguelsantiagoews@gmail.com>
2024-11-17 16:44:49 -0600

    AudioMixer
    Created audio mixer
    * Added type of sounds

---
+++ Assets/Sounds/AudioManager.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header ("Audio Source")]
+    public AudioSource musicSource;
+    public AudioSource SFXSource;
+    [SerializeField] AudioMixer mixer;
+
+    [Header("Audio Clip")]
+    public AudioClip background;
+    public AudioClip death;
+
+    [Header("VolumeSliders")]
+    public Slider masterVolumeSlider;
+    public Slider musicVolumeSlider;
+    public Slider sfxVolumeSlider;
+
+    float volumeSet = 1;
+
+    private void Start()
+    {
+        musicSource.clip = background;
+        musicSource.Play();
+
+        if (masterVolumeSlider != null)
+        {
+            masterVolumeSlider.onValueChanged.AddListener(ChangeMasterVolume);
+        }
+
+        if (musicVolumeSlider != null)
+        {
+            musicVolumeSlider.onValueChanged.AddListener(ChangeMusicVolume);
+        }
+
+        if (sfxVolumeSlider != null)
+        {
+            sfxVolumeSlider.onValueChanged.AddListener(ChangeSFXVolume);
+        }
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        SFXSource.PlayOneShot(clip);
+    }
+
+    public void ChangeMasterVolume(float sliderValue)
+    {
+        // Convertimos el rango del slider (0 a 1) a un rango de dB (-80 a 0)
+        float volumeInDB = (1 - Mathf.Sqrt(sliderValue)) * -80f;
+        mixer.SetFloat("MasterVolume", volumeInDB);
+        Debug.Log($"Volumen ajustado a: {volumeInDB} dB con valor del slider: {sliderValue}");
+    }
+
+    public void ChangeMusicVolume(float sliderValue)
+    {
+        // Convertimos el rango del slider (0 a 1) a un rango de dB (-80 a 0)
+        float volumeInDB = (1 - Mathf.Sqrt(sliderValue)) * -80f;
+        mixer.SetFloat("MusicVolume", volumeInDB);
+        Debug.Log($"Volumen ajustado a: {volumeInDB} dB con valor del slider: {sliderValue}");
+    }
+
+    public void ChangeSFXVolume(float sliderValue)
+    {
+        // Convertimos el rango del slider (0 a 1) a un rango de dB (-80 a 0)
+        float volumeInDB = (1 - Mathf.Sqrt(sliderValue)) * -80f;
+        mixer.SetFloat("SFXVolume", volumeInDB);
+        Debug.Log($"Volumen ajustado a: {volumeInDB} dB con valor del slider: {sliderValue}");
+    }
+
+}
